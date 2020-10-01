4f638a3c1ad4db1a2e0ed7a5c65601bcb784e60b
dipa2000 <dipa2000.pal2@gmail.com>
2020-10-01 21:50:45 +0530

    added new method

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello{
     public static void main(string[]gs args){
         System.out.println("vscode is awesome")
+
+        greeting("hi everyone")
     }
+
+    public static void greating(string name)
+        System.out.println("welcome" + name)
 }
